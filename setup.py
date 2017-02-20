bae74b30147c29a54eb76fa06e3b08de4036c07f
Jens Stimpfle <jfs@jstimpfle.de>
2017-02-21 00:01:12 +0100

    bump version to 0.12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='wsl',
-    version='0.11',
+    version='0.12',
     description='Library for reading and writing WSL databases',
     long_description='Library for reading and writing WSL databases',
     url='http://jstimpfle.de/projects/wsl/main.html',
